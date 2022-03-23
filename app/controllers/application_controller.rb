36f3b13b5e6f440d626f93a16cf41f1228caafff
ivopozzani <ivo.pozzani@gmail.com>
2022-03-23 15:54:08 -0300

    feat: added authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Token::ControllerMethods
   include ErrorHandler
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_token do |token|
+      ActiveSupport::SecurityUtils.secure_compare(token, 'prize_draw_authorization')
+    end
+  end
 end
