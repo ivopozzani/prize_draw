a55b956bdbea65695178fb8263264e84906010cf
ivopozzani <ivo.pozzani@gmail.com>
2022-03-25 14:48:18 -0300

    feat: updated api docs

--- spec/requests/v1/people_spec.rb
+++ spec/requests/v1/people_spec.rb
@@ -1,11 +1,23 @@
+# frozen_string_literal: true
+
 require 'swagger_helper'
 
 RSpec.describe 'v1/people', type: :request do
-
   path '/v1/people' do
-
     get('list people') do
       response(200, 'successful') do
+        examples 'application/json' => [{
+          id: 1,
+          name: 'Richard Wilson',
+          cpf: '333.333.888-99',
+          birth_date: '2021-03-22'
+        },
+                                        {
+                                          id: 2,
+                                          name: 'Maria Candida',
+                                          cpf: '222.333.222-99',
+                                          birth_date: '2022-03-22'
+                                        }]
 
         after do |example|
           example.metadata[:response][:content] = {
@@ -19,7 +31,23 @@ RSpec.describe 'v1/people', type: :request do
     end
 
     post('create person') do
-      response(200, 'successful') do
+      consumes 'application/json'
+      parameter name: :blog, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string },
+          cpf: { type: :string },
+          birth_date: { type: :string }
+        },
+        required: %w[name cpf]
+      }
+
+      response(201, 'created') do
+        examples 'application/json' => {
+          name: 'Richard Wilson',
+          cpf: '333.333.888-99',
+          birth_date: '2021-03-22'
+        }
 
         after do |example|
           example.metadata[:response][:content] = {
@@ -38,8 +66,22 @@ RSpec.describe 'v1/people', type: :request do
     parameter name: 'id', in: :path, type: :string, description: 'id'
 
     patch('update person') do
+      consumes 'application/json'
+      parameter name: :blog, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string },
+          cpf: { type: :string },
+          birth_date: { type: :string }
+        }
+      }
+
       response(200, 'successful') do
         let(:id) { '123' }
+
+        examples 'application/json' => {
+          name: 'Maria Candida'
+        }
 
         after do |example|
           example.metadata[:response][:content] = {
@@ -53,8 +95,24 @@ RSpec.describe 'v1/people', type: :request do
     end
 
     put('update person') do
+      consumes 'application/json'
+      parameter name: :blog, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string },
+          cpf: { type: :string },
+          birth_date: { type: :string }
+        }
+      }
+
       response(200, 'successful') do
         let(:id) { '123' }
+
+        examples 'application/json' => {
+          name: 'Richard Wilson',
+          cpf: '333.333.888-99',
+          birth_date: '2021-03-22'
+        }
 
         after do |example|
           example.metadata[:response][:content] = {
--- spec/requests/v1/prizes_spec.rb
+++ spec/requests/v1/prizes_spec.rb
@@ -1,11 +1,21 @@
+# frozen_string_literal: true
+
 require 'swagger_helper'
 
 RSpec.describe 'v1/prizes', type: :request do
-
   path '/v1/people/prizes' do
-
     get('list prizes') do
       response(200, 'successful') do
+        examples 'application/json' => [{
+          name: 'Richard Wilson',
+          cpf: '333.333.888-99',
+          drawn_at: '2022-03-24T19:45:00.950Z'
+        },
+                                        {
+                                          name: 'Maria Candida',
+                                          cpf: '222.333.222-99',
+                                          drawn_at: '2022-03-24T18:56:00.950Z'
+                                        }]
 
         after do |example|
           example.metadata[:response][:content] = {
@@ -19,7 +29,13 @@ RSpec.describe 'v1/prizes', type: :request do
     end
 
     post('create prize') do
+      parameter name: 'Authorization', in: :header, type: :string
       response(200, 'successful') do
+        examples 'application/json' => {
+          name: 'Richard Wilson',
+          cpf: '333.333.888-99',
+          drawn_at: '2022-03-24T19:45:00.950Z'
+        }
 
         after do |example|
           example.metadata[:response][:content] = {
